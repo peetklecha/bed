68369f1ea2b91ba457a39fe868e6d9e3a7568ca8
Peet Klecha <p.a.klecha@gmail.com>
2022-02-06 11:38:28 -0500

    WIP

--- cookie.ts
+++ cookie.ts
@@ -1,1 +1,8 @@
 import { getCookies, setCookie } from 'https://deno.land/std@0.125.0/http/cookie.ts'
+import type { Handler } from "./lib/types.ts";
+
+const processCookies : Handler = ctx => {
+	const cookies = getCookies(ctx.requestEvent.request.headers);
+	ctx.cookies = cookies;
+	ctx.next();
+}
